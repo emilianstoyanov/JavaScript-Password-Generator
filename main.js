53d7dde3b0360b0dc8bcb05e67c8ca01a4af50cf
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-09-14 15:13:52 +0300

    generate password and random func

--- main.js
+++ main.js
@@ -1,3 +1,58 @@
+// DOM elements
+const resultEl = document.getElementById('result')
+const lengthtEl = document.getElementById('length')
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const generateEl = document.getElementById('generate')
+const clipboardEl = document.getElementById('clipboard')
+
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+};
+
+
+// Generate event listen
+generateEl.addEventListener('click', () => {
+    const length = +lengthtEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+
+
+     resultEl.innerText = generatePassword(
+        hasLower, 
+        hasUpper, 
+        hasNumber, 
+        hasSymbol, 
+        length)
+
+});
+
+// Generate password function
+function generatePassword(lower, upper, number, symbols, length) {
+
+    let generatedPassword = '';
+
+    const typesCount = lower + upper + number + symbols;
+
+    console.log(typesCount)
+
+    const typeArr = [{ lower }, { upper }, { number }, { symbols }]
+
+    console.log(typeArr)
+
+
+}
+
+
+// Generator functions 
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
@@ -15,10 +70,6 @@ function getRandomSymbol() {
     return symbol[Math.floor(Math.random() * symbol.length)]
 }
 
-console.log(getRandomUpper())
-console.log(getRandomLower())
-console.log(getRandomNumber())
-console.log(getRandomSymbol())
 
 
 
